bfcf24650cb1dad38a7098e003b549e62b5e0b23
willAtaides <sg552.santos@gmail.com>
2025-07-11 05:16:36 -0300

    Iniciando projeto crud initalizr

---
+++ src/main/java/com/onvex/crudclient/CrudclientApplication.java
@@ -0,0 +1,13 @@
+package com.onvex.crudclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudclientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onvex/crudclient/CrudclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onvex.crudclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
