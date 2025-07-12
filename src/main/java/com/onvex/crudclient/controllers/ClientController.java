c0167a342053b4d0ef2f6958f9d85b6824adf09a
willAtaides <sg552.santos@gmail.com>
2025-07-12 15:23:18 -0300

    POST new client

--- src/main/java/com/onvex/crudclient/controllers/ClientController.java
+++ src/main/java/com/onvex/crudclient/controllers/ClientController.java
@@ -7,11 +7,11 @@ import com.onvex.crudclient.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,11 +22,21 @@ public class ClientController {
     private ClientService service;
 
     @GetMapping(value = "/{id}")
-    public ClientDTO findById(@PathVariable Long id){
-        return service.findByid(id);
+    public ResponseEntity<ClientDTO> findById(@PathVariable Long id){
+        ClientDTO dto= service.findByid(id);
+        return ResponseEntity.ok(dto);
     }
     @GetMapping
-    public Page<ClientDTO> findAll(Pageable pageable){
-        return service.findAll(pageable);
+    public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable){
+        Page<ClientDTO> dto= service.findAll(pageable);
+        return ResponseEntity.ok(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto){
+        dto= service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 }
--- src/main/java/com/onvex/crudclient/services/ClientService.java
+++ src/main/java/com/onvex/crudclient/services/ClientService.java
@@ -30,4 +30,17 @@ public class ClientService {
         return result.map(x -> new ClientDTO(x));
     }
 
+    @Transactional
+    public ClientDTO insert(ClientDTO dto){
+        Client entity = new Client();
+        entity.setName(dto.getName());
+        entity.setCpf(dto.getCpf());
+        entity.setIncome(dto.getIncome());
+        entity.setBirthDate(dto.getBirthDate());
+        entity.setChildren(dto.getChildren());
+
+        entity = repository.save(entity);
+        return new ClientDTO(entity);
+    }
+
 }
